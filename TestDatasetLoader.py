8a89e9c361950a6c512f74750abd783366da0db3
NevesLucas <neves.lucas.m@gmail.com>
2022-10-23 15:27:37 -0400

    write data loading tests, fix runtime issues, new deps

--- TestDatasetLoader.py
+++ TestDatasetLoader.py
@@ -1,1 +1,43 @@
 #unit test the dataset loader to make sure its working properly
+
+import kaggleDataLoader
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+dataLoader = kaggleDataLoader.KaggleDataLoader()
+
+testID = dataLoader.listTrainPatientID()[0]
+breaks = dataLoader.fracturedBones(testID)
+print(breaks)
+
+cv, imgSlice = dataLoader.loadSliceImageFromId(testID, 1)
+
+imSlicePxArr = imgSlice.pixel_array
+fig, ax = plt.subplots(1,1, figsize=(10, 10))
+
+ax.imshow(imSlicePxArr)
+plt.show()
+
+id1 = '1.2.826.0.1.3680043.780'
+
+segment = dataLoader.loadSegmentationsForPatient(id1)
+temp = segment[0,:,:]
+for i in range(segment.shape[0]):
+    temp = temp + segment[i,:,:]
+
+fig, ax = plt.subplots(1,1, figsize=(10, 10))
+ax.imshow(temp)
+plt.show()
+
+id1 = '1.2.826.0.1.3680043.10051'
+slice_number = 136
+
+cv, imgSlice = dataLoader.loadSliceImageFromId(id1, slice_number)
+imSlicePxArr = imgSlice.pixel_array
+bboxes = dataLoader.bboxFromIndex(id1, slice_number)
+rect = patches.Rectangle((bboxes[1], bboxes[2]), bboxes[3], bboxes[4], linewidth=2, edgecolor='r', facecolor="none")
+fig, ax = plt.subplots(1,1, figsize=(10, 10))
+ax.imshow(imSlicePxArr)
+ax.add_patch(rect)
+plt.show()
--- kaggleDataLoader.py
+++ kaggleDataLoader.py
@@ -9,7 +9,7 @@ import json
 import cv2
 import os
 from tqdm import tqdm
-
+import random
 with open('config.json', 'r') as f:
     paths = json.load(f)
 
@@ -17,13 +17,30 @@ RSNA_2022_PATH    = paths["RSNA_2022_PATH"]
 TRAIN_IMAGES_PATH = f'{RSNA_2022_PATH}/train_images'
 TEST_IMAGES_PATH  = f'{RSNA_2022_PATH}/test_images'
 
+
+target_cols = ['C1', 'C2', 'C3',
+               'C4', 'C5', 'C6', 'C7',
+               'patient_overall']
+
+
+def loadDicom(path):
+    img = pydicom.dcmread(path)
+    img.PhotometricInterpretation = 'YBR_FULL'
+    data = img.pixel_array
+    data = data - np.min(data)
+    if np.max(data) != 0:
+        data = data / np.max(data)
+    data = (data * 255).astype(np.uint8)
+    return cv2.cvtColor(data, cv2.COLOR_GRAY2RGB), img
+
+
 class KaggleDataLoader:
 
     def __init__(self):
         # Load metadata
 
         self.trainImagePath = TRAIN_IMAGES_PATH
-        self.segPath        = os.path.join(TRAIN_IMAGES_PATH,          "segmentations")
+        self.segPath        = os.path.join(RSNA_2022_PATH,          "segmentations")
         self.trainDf        = pd.read_csv(os.path.join(RSNA_2022_PATH, "train.csv"))
         self.trainBbox      = pd.read_csv(os.path.join(RSNA_2022_PATH, "train_bounding_boxes.csv"))
         self.testDf         = pd.read_csv(os.path.join(RSNA_2022_PATH, "test.csv"))
@@ -47,12 +64,12 @@ class KaggleDataLoader:
         self.trainDf.head()
 
     def bboxFromIndex(self, id, sliceNum):
-        box = self.trainBbox.loc[(self.trainBbox.StudyInstanceUID == id & self.trainBbox.slice_number == sliceNum), :]
+        box = self.trainBbox.loc[(self.trainBbox.StudyInstanceUID == id) & (self.trainBbox.slice_number == sliceNum), :]
         return list(box.values[0])
 
-    def fracturedBones(self, patientID):
+    def fracturedBones(self, id):
         fractured_bones = []
-        temp = self.trainDf.loc[self.trainDf.StudyInstanceUID == patientID, ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7']]
+        temp = self.trainDf.loc[self.trainDf.StudyInstanceUID == id, ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7']]
         temp = list(temp.values[0])  # there is one row per id
         for i in range(len(temp)):
             if temp[i] == 1:
@@ -65,24 +82,14 @@ class KaggleDataLoader:
     def listTestPatientID(self):
         return list(self.testDf["StudyInstanceUID"])
 
-
-    def loadDicom(self, path):
-        img = pydicom.dcmread(path)
-        img.PhotometricInterpretation = 'YBR_FULL'
-        data = img.pixel_array
-        data = data - np.min(data)
-        if np.max(data) != 0:
-            data = data / np.max(data)
-        data = (data * 255).astype(np.uint8)
-        return cv2.cvtColor(data, cv2.COLOR_GRAY2RGB), img
-
     def loadSliceImageFromId(self, patientID, sliceIndex):
         imgPath = self.trainDf.loc[self.trainDf.StudyInstanceUID == patientID, "img_paths"]
-        targetPath = os.path.join(imgPath, str(sliceIndex), ".dcm")
-        return self.loadDicom(targetPath)
+        imgPath = imgPath.iloc[0]
+        targetPath = os.path.join(imgPath, str(sliceIndex)+".dcm")
+        return loadDicom(targetPath)
 
     def loadSegmentationsForPatient(self, patientID):
-        segmentations = nib.load(os.path.join(self.segPath, patientID, '.nii')).get_fdata()
+        segmentations = nib.load(os.path.join(self.segPath, patientID+'.nii')).get_fdata()
         segmentations = segmentations[:, ::-1, ::-1]
         segmentations = segmentations.transpose(2, 1, 0)
         return segmentations
