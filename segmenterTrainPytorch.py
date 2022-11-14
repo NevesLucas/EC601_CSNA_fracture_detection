28eb4c6d21fa1db0ebcc1c4b3f32bdcb9f67fa19
NevesLucas <neves.lucas.m@gmail.com>
2022-11-14 13:38:42 -0500

    lower batch size due to OOM

--- segmenterTrainPytorch.py
+++ segmenterTrainPytorch.py
@@ -66,7 +66,7 @@ train, val = dataset.loadDatasetAsSegmentor()
 train = cachingDataset(train)
 val = cachingDataset(val)
 train_loader = DataLoader(
-    train, batch_size=4, shuffle=True, prefetch_factor=4, persistent_workers=True, drop_last=True, num_workers=16)
+    train, batch_size=1, shuffle=True, prefetch_factor=4, persistent_workers=True, drop_last=True, num_workers=16)
 
 val_loader = DataLoader(
     val, batch_size=1, num_workers=8)
