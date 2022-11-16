549d9ff6bbeb8d4e366b5621dde49be8587e1a76
NevesLucas <neves.lucas.m@gmail.com>
2022-11-15 20:52:02 -0500

    fix

--- kaggleDataLoader.py
+++ kaggleDataLoader.py
@@ -158,12 +158,13 @@ class KaggleDataLoader:
             rescale,
         ])
         normalize_orientation = tio.ToCanonical()
-        downsample = tio.Resample(1), tio.Resample('t1')
-
+        downsample = tio.Resample(1)
+        resample = tio.Resample('t1')
         cropOrPad = tio.CropOrPad((130,130,200))
         preprocess_spatial = tio.Compose([
             normalize_orientation,
             downsample,
+            resample,
             cropOrPad,
         ])
         sequential = tio.SequentialLabels()
