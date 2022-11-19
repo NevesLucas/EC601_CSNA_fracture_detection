db34f4a2e4fa16af2d954643e4af135eeefa0a6d
NevesLucas <neves.lucas.m@gmail.com>
2022-11-19 11:20:22 -0500

    amp gives more vram, try larger unet

--- segmenterTrainPytorch.py
+++ segmenterTrainPytorch.py
@@ -11,7 +11,7 @@ import pandas as pd
 from monai.data import decollate_batch, DataLoader,Dataset,ImageDataset
 from monai.metrics import ROCAUCMetric
 from monai.losses.dice import DiceLoss
-from monai.networks.nets import VNet
+from monai.networks.nets import BasicUNetPlusPlus
 import torch.cuda.amp as amp
 import torchio as tio
 
@@ -61,7 +61,7 @@ val_loader = DataLoader(
     val, batch_size=1, num_workers=8)
 
 N_EPOCHS = 500
-model = VNet(spatial_dims=3,
+model = BasicUNetPlusPlus(spatial_dims=3,
                   in_channels=1,
                   out_channels=1).to(device)
 
