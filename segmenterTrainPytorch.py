e41d81c01bf844e57a384cfd7602f71a11054603
NevesLucas <neves.lucas.m@gmail.com>
2022-12-03 18:48:12 -0500

    more cores

--- segmenterTrainPytorch.py
+++ segmenterTrainPytorch.py
@@ -60,7 +60,7 @@ train, val = dataset.loadDatasetAsSegmentor(trainPercentage=0.80)
 train = cachingDataset(train)
 val = cachingDataset(val)
 train_loader = DataLoader(
-    train, batch_size=1, shuffle=True, prefetch_factor=4, persistent_workers=True, drop_last=True, num_workers=8)
+    train, batch_size=1, shuffle=True, prefetch_factor=4, persistent_workers=True, drop_last=True, num_workers=16)
 
 val_loader = DataLoader(
     val, batch_size=1, num_workers=8)
